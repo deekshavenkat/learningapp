4b3448abb137120f0a6e0e4ee84255428d34e645
Deekshitha V <97940013+deekshavenkat@users.noreply.github.com>
2022-07-12 22:47:00 +0530

    Update users.js

--- Nodejs/src/routers/users.js
+++ Nodejs/src/routers/users.js
@@ -1,7 +1,3 @@
-
-
-
-
 const express = require("express");
 const Course = require("../mongoose/models/courses");
 
@@ -9,86 +5,90 @@ const Course = require("../mongoose/models/courses");
 const usersRouter = new express.Router();
 
 //write your code here
-usersRouter.get("/courses/get",async(req, res)=>{
+usersRouter.get("/courses/get", async (req, res) => {
     await Course.find()
-    .then((resp)=>{
-        res.status(200).json(resp)
-    })
-    .catch(()=>{
-        res.status(400)
-    })
+        .then((resp) => {
+            res.status(200).json(resp)
+        })
+        .catch(() => {
+            res.status(400)
+        })
 })
 
-usersRouter.post("/courses/enroll/:id",(req, res)=>{
+usersRouter.post("/courses/enroll/:id", (req, res) => {
     const id = req.params.id
-     Course.findById(id)
-     .then((course)=>{
-        if(course.isApplied=== true){
-            res.status(403).json({
-                error:"You have already applied for this course"
-            })
-        }
-        else{
-            course.isApplied = true
-            course.save().then(()=>{
-                res.status(200).json({
-                    message:"You have successfully enrolled fo this course "
+    Course.findById(id)
+        .then((course) => {
+            if (course.isApplied === true) {
+                res.status(403).json({
+                    error: "You have already applied for this course"
                 })
-            })
-        }
-     })
+            }
+            else {
+                course.isApplied = true
+                course.save().then(() => {
+                    res.status(200).json({
+                        message: "You have successfully enrolled fo this course "
+                    })
+                })
+            }
+        })
 
-    
+
+
 })
 
 
-usersRouter.delete("/courses/drop/:id", (req, res)=>{
-    Course.findById(req.params.id).then((course)=>{
-        if(course.isApplied === true){
+usersRouter.delete("/courses/drop/:id", (req, res) => {
+    Course.findById(req.params.id).then((course) => {
+        if (course.isApplied === true) {
             course.isApplied = false
             course.save()
-            .then(()=>{
-                res.status(200).json({
-                    message: "You have dropped the course"
+                .then(() => {
+                    res.status(200).json({
+                        message: "You have dropped the course"
+                    })
                 })
-            })
-        }else{
-            res.ststus(403).json({
-                error:"You have not enrolled for this course"
+        } else {
+            res.status(403).json({
+                error: "You have not enrolled for this course"
             })
         }
     })
 })
 
-usersRouter.patch("/courses/rating/:id", (req, res)=>{
+usersRouter.patch("/courses/rating/:id", (req, res) => {
     Course.findById(req.params.id)
-    .then((course)=>{
-        if(course.isApplied== false){
-            res.status(403).json({
-                error:"You have not enrolled for this  course"
-            })
-        }
-
-       else  if(course.isRated === true){
-            res.status(403).json({
-                error:"You have already rated this course"
-            })
-        }else {
-            var newRating = parseInt(req.body.rating)
-            var oldnoOfRatings = course.noOfRatings;
-            var makeRating = ((course.rating*oldnoOfRatings)+ newRating)/(oldnoOfRatings+ 1)
-            course.rating = makeRating.toFixed(1);
-            course.save().then(()=>{
-                res.status(200).json({
-                    message: "You have rated this course "
+        .then((course) => {
+            if (course.isApplied == false) {
+                res.status(403).json({
+                    error: "You have not enrolled for this course"
                 })
-            })
-            
-        }
-    })
-    .catch((err)=>{
-        res.status(400)
-        console.log(err)
-    })
+            }
+
+            else if (course.isRated === true) {
+                res.status(403).json({
+                    error: "You have already rated this course"
+                })
+            } else {
+                var newRating = parseInt(req.body.rating)
+                var oldnoOfRatings = course.noOfRatings;
+                var makeRating = ((course.rating * oldnoOfRatings) + newRating) / (oldnoOfRatings + 1)
+                course.rating = makeRating.toFixed(1);
+                course.noOfRatings = oldnoOfRatings + 1
+                course.isRated = true
+                course.save().then(() => {
+                    res.status(200).json({
+                        message: "You have rated this course "
+                    })
+                })
+
+
+            }
+        })
+        .catch((err) => {
+            res.status(400)
+            console.log(err)
+        })
 })
 module.exports = usersRouter;
